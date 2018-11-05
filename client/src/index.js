01bc3f06086925c039e3f642357e3fce2080a812
yauheni_chyrkouski <evgen.box@live.ru>
2018-11-05 16:46:42 +0300

    redux added

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './app';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app')
 );
 
